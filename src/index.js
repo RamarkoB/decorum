4f7d45ea563b25c25d682947f422e3f19fab9cf1
RamarkoB <ramarkob@uchicago.edu>
2023-01-30 17:56:38 -0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App, Footer} from './App';
+import reportWebVitals from './prebuilt/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+const app = ReactDOM.createRoot(document.getElementById('root')); 
+const footer = ReactDOM.createRoot(document.getElementById('footer')); 
+
+setInterval(function() {
+  app.render(<App />)
+  footer.render(<Footer />);
+}, 50);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
